bde3c88b677df4d20d6486bf69ab2023c5b804ad
Fernando Soares <fernandosoares@192.168.15.16>
2026-09-19 18:42:55 -0300

    Criando a funcionalidade de cancelamento de caronas

--- src/main/java/br/ifrn/caronas/dtos/CaronaItemListaDTO.java
+++ src/main/java/br/ifrn/caronas/dtos/CaronaItemListaDTO.java
@@ -1,8 +1,11 @@
 package br.ifrn.caronas.dtos;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
+import br.ifrn.caronas.models.Carona;
+import br.ifrn.caronas.models.Usuario;
 import lombok.Builder;
 import lombok.Data;
 
@@ -17,10 +20,55 @@ public class CaronaItemListaDTO {
 	private Double valor;
 	private String observacoes;
 	private String motorista;
+	private String motoristaTelefone;
 	private boolean cancelada;
 	private boolean estouNaCarona;
 	private boolean souMotorista;
 	private boolean esgotada;
 	private List<String> passageiros;
+	
+	public static List<CaronaItemListaDTO> gerarCaronaItemListaDTO(List<Carona> caronas, Usuario usuario) {
+		List<CaronaItemListaDTO> dtos = new ArrayList<CaronaItemListaDTO>();
+
+		for (Carona c : caronas) {
+			dtos.add(gerarCaronaItemListaDTO(c, usuario));
+		}
+
+		return dtos;
+	}
+
+	public static CaronaItemListaDTO gerarCaronaItemListaDTO(Carona carona, Usuario usuario) {
+		CaronaItemListaDTO dto = CaronaItemListaDTO.builder()
+									.id(carona.getId())
+									.direcao(carona.getDirecao())
+									.cancelada(carona.isCancelada())
+									.data(carona.getData())
+									.motorista(carona.getMotorista().getNome())
+									.motoristaTelefone(carona.getMotorista().getTelefone())
+									.observacoes(carona.getObservacoes())
+									.vagas(carona.getVagas())
+									.valor(carona.getValor())
+									.build();
+
+		if (carona.getMotorista().getId() == usuario.getId()) {
+			dto.setEstouNaCarona(true);
+			dto.setSouMotorista(true);
+		} else {
+			for (Usuario p : carona.getPassageiros()) {
+				if (p.getId() == usuario.getId()) {
+					dto.setEstouNaCarona(true);
+					break;
+				}
+			}
+		}
+
+		dto.setPassageiros(new ArrayList<String>());
+		for (Usuario p : carona.getPassageiros()) {
+			dto.getPassageiros().add(p.getNome());
+		}
+
+		dto.setEsgotada(carona.getVagas() == carona.getPassageiros().size());
+		return dto;
+	}
 	
 }
