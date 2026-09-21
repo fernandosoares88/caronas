ebe427025223812bd85889604130b51a95a66660
Fernando Soares <fernandosoares@MacBook-Air-de-Fernando-2.local>
2026-09-21 19:53:53 -0300

    Resolvendo inconsistências de Layout

--- src/main/java/br/ifrn/caronas/CaronasApplication.java
+++ src/main/java/br/ifrn/caronas/CaronasApplication.java
@@ -18,7 +18,6 @@ public class CaronasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CaronasApplication.class, args);
-		System.out.println("iniciou a aplicação");
 	}
 
 }
