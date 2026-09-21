2f96bbe91100af18d36a620688104e262d013c49
Fernando Soares <fernandosoares@MacBook-Air-de-Fernando-2.local>
2026-09-21 09:26:49 -0300

    Testando ajuste de data

--- src/main/java/br/ifrn/caronas/CaronasApplication.java
+++ src/main/java/br/ifrn/caronas/CaronasApplication.java
@@ -1,10 +1,20 @@
 package br.ifrn.caronas;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class CaronasApplication {
+	
+	@PostConstruct
+    public void init() {
+        // Define o fuso horário padrão para o horário de Brasília (ajuste se necessário)
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Fortaleza"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CaronasApplication.class, args);
