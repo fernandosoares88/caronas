6daddca5345fdb6c0046f0f71f8beb236992150f
Fernando Soares <fernandosoares@MacBook-Air-de-Fernando-2.local>
2026-09-25 13:48:50 -0300

    Funcionalidade de edição de caronas e melhorias o layout

--- src/main/java/br/ifrn/caronas/dtos/CaronaFormDTO.java
+++ src/main/java/br/ifrn/caronas/dtos/CaronaFormDTO.java
@@ -2,16 +2,19 @@ package br.ifrn.caronas.dtos;
 
 import java.time.LocalDateTime;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import br.ifrn.caronas.models.Carona;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-public class CaronaRequestDTO {
+public class CaronaFormDTO {
 	
 	private Long id;
 	private String direcao;
+	@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
 	private LocalDateTime data;
 	private Integer vagas;
 	private Double valor;
@@ -25,6 +28,17 @@ public class CaronaRequestDTO {
 				.vagas(this.vagas)
 				.valor(this.valor)
 				.observacoes(this.observacoes)
+				.build();
+	}
+	
+	public static CaronaFormDTO gerarCaronaRequestDTO(Carona carona) {
+		return CaronaFormDTO.builder()
+				.id(carona.getId())
+				.direcao(carona.getDirecao())
+				.data(carona.getData())
+				.vagas(carona.getVagas())
+				.valor(carona.getValor())
+				.observacoes(carona.getObservacoes())
 				.build();
 	}
 
